17a2ac4ef59084ad2b371adf9328123bd99d35e5
Dev <Dev@devs-macbook-pro.local>
2017-10-05 10:36:59 +0800

    Initial Commit

---
+++ MySplitView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySplitView
+//
+//  Created by Dev on 5/10/17.
+//  Copyright © 2017 ITE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySplitView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySplitView
+//
+//  Created by Dev on 5/10/17.
+//  Copyright © 2017 ITE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
